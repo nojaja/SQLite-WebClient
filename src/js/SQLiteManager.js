26b342054d159beb9f58b7341468f5565ec25258
nojaja <free.riccia@gmail.com>
2025-04-15 18:15:32 +0900

    import/exportが出来なくなっていたので修正

--- src/js/SQLiteManager.js
+++ src/js/SQLiteManager.js
@@ -8,34 +8,26 @@ class SQLiteManager {
             print: options.print || (() => { }),
             printErr: options.printErr || (() => { })
         });
-        const sqlite3_instance = new SQLiteManager(sqlite3, data);
-        // sqlite3_instanceにoriginalプロパティを作成
-        sqlite3_instance.original = { db: {} };
 
-        // 元のprepareメソッドを保存
-        sqlite3_instance.original.db.prepare = sqlite3_instance.db.prepare;
-        sqlite3_instance.original.db.exec = sqlite3_instance.db.exec;
-
-        // db.prepareをオーバーライド
-        sqlite3_instance.db.prepare = function (sql) {
-            return sqlite3_instance.prepare(sql);
-        };
-        // db.execをオーバーライド
-        sqlite3_instance.db.exec = function (sql, bind) {
-            return sqlite3_instance.exec(sql, bind);
-        };
+        const sqlite3_instance = new SQLiteManager(sqlite3, options);
+        await sqlite3_instance.setupEnvironment(data);
         return sqlite3_instance;
     }
 
-    constructor(sqlite3, data, options = {}) {
+    constructor(sqlite3, options = {}) {
         this.print = options.print || (() => { });
         this.printErr = options.printErr || (() => { });
         this.sqlite3 = sqlite3;
+        this.original = { db: {} };
+        this.db = null;
+    }
+
+    async setupEnvironment(data) {
         // ファイル名生成
         this.currentFilename = "dbfile_" + (0xffffffff * Math.random() >>> 0);
         if (data) {
             // VFSを使用してデータをインポート
-            sqlite3.capi.sqlite3_js_vfs_create_file(
+            this.sqlite3.capi.sqlite3_js_vfs_create_file(
                 'unix',
                 this.currentFilename,
                 data,
@@ -43,39 +35,46 @@ class SQLiteManager {
             );
         }
         // データベースを作成
-        this.db = new sqlite3.oo1.DB(this.currentFilename, "ct");
-    }
+        this.db = new this.sqlite3.oo1.DB(this.currentFilename, "ct");
+        // sqlite3_instanceにoriginalプロパティを作成
+        this.original = { db: {} };
 
-    exec(sql, bind) {
-        const results = [];
-        let columnNames = [];
-        try {
-            this.original.db.exec.call(this.db, {
-                sql: sql,
-                bind: bind,
-                rowMode: 'object',
-                callback: (row) => {
-                    columnNames = Array.from(new Set([...columnNames, ...Object.keys(row)]));
-                    results.push(Object.values(row));
-                }
-            });
-            return [{
-                columns: columnNames,
-                values: results
-            }];
-        } catch (error) {
-            throw error;
-        }
-    }
+        // 元のprepareメソッドを保存
+        this.original.db.prepare = this.db.prepare;
+        this.original.db.exec = this.db.exec;
 
-    prepare(sql) {
-        // 元のprepareメソッドを呼び出し
-        const stmt = this.original.db.prepare.call(this.db, sql);
-        // SQLiteManagerのカスタマイズを適用
-        stmt.getRowAsObject = () => this.getRowAsObject.call(this, stmt);
-        stmt.getAsObject = () => this.getRowAsObject.call(this, stmt); //sql.js
+        // db.prepareをオーバーライド
+        this.db.prepare = (sql) => {
+            // 元のprepareメソッドを呼び出し
+            const stmt = this.original.db.prepare.call(this.db, sql);
+            // SQLiteManagerのカスタマイズを適用
+            stmt.getRowAsObject = () => this.getRowAsObject.call(this, stmt);
+            stmt.getAsObject = () => this.getRowAsObject.call(this, stmt); //sql.js
 
-        return stmt;
+            return stmt;
+        };
+        // db.execをオーバーライド
+        this.db.exec = (sql, bind) => {
+            const results = [];
+            let columnNames = [];
+            try {
+                this.original.db.exec.call(this.db, {
+                    sql: sql,
+                    bind: bind,
+                    rowMode: 'object',
+                    callback: (row) => {
+                        columnNames = Array.from(new Set([...columnNames, ...Object.keys(row)]));
+                        results.push(Object.values(row));
+                    }
+                });
+                return [{
+                    columns: columnNames,
+                    values: results
+                }];
+            } catch (error) {
+                throw error;
+            }
+        };
     }
 
     // ヘルパーメソッド：行データをオブジェクトとして取得
@@ -95,11 +94,7 @@ class SQLiteManager {
 
     async import(contents) {
         this.db.close();
-        const vfsName = 'unix'; // 使用するVFSの名前
-        this.currentFilename = "dbfile_" + (0xffffffff * Math.random() >>> 0);
-
-        this.sqlite3.capi.sqlite3_js_vfs_create_file(vfsName, this.currentFilename, contents, contents.length);
-        this.db = new this.sqlite3.oo1.DB(this.currentFilename);
+        await this.setupEnvironment(contents);
     }
 
     close() {
